5c1d3af078481a0bad9031aafe146da07eed1501
Yunus <vokhstag@gmail.com>
2021-10-26 14:22:02 +0300

    Добавил AuthorizationModule
    Добавил AuthorizationModule и ModelBuilder

--- CookingRecipes/Helpers/ModelBuilder.swift
+++ CookingRecipes/Helpers/ModelBuilder.swift
@@ -5,4 +5,16 @@
 //  Created by Yunus Abubakarov on 26.10.2021.
 //
 
-import Foundation
+import UIKit
+
+protocol Builder {
+    static func createAuthorizationModule() -> UIViewController
+}
+
+class ModelBuilder: Builder {
+    static func createAuthorizationModule() -> UIViewController {
+        let view = AuthorizationViewController()
+        // TODO: - Добавить презентер и настройки
+        return view
+    }
+}
