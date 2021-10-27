bae92ed05895d58f21770238432cab887d027846
Yunus <vokhstag@gmail.com>
2021-10-27 10:49:04 +0300

    Заполнил AssemblyBuilder
    Добавил в AssemblyBuilder методы позволяющие получать различные контроллеры

---
+++ CookingRecipes/📱 Screens/FavoriteFoodModule/View/FavoriteFoodViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FavoriteFoodViewController.swift
+//  CookingRecipes
+//
+//  Created by Yunus Abubakarov on 27.10.2021.
+//
+
+import UIKit
+
+class FavoriteFoodViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
