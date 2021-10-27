c58f28496503840c58bae2b6722e6039bc56547a
Yunus <vokhstag@gmail.com>
2021-10-27 10:21:06 +0300

    Добавил FavoriteFoodRouter
    Добавил FavoriteFoodRouter

---
+++ CookingRecipes/📱 Screens/FavoriteFoodModule/FavoriteFoodRouter.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteFoodRouter.swift
+//  CookingRecipes
+//
+//  Created by Yunus Abubakarov on 27.10.2021.
+//
+
+import Foundation
+
+protocol FavoriteFoodRouterProtocol {
+    
+}
+
+class FavoriteFoodRouter {
+    
+}
