2764776bb74e41f1ab6b4bebafa2391dde50958d
Yunus <vokhstag@gmail.com>
2021-10-27 10:29:52 +0300

    Добавил TabBarRouter
    Добавил TabBarRouter который будет осуществлять навигацию по TabBar

---
+++ CookingRecipes/📱 Screens/TabBarRouter.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarRouter.swift
+//  CookingRecipes
+//
+//  Created by Yunus Abubakarov on 27.10.2021.
+//
+
+import Foundation
+
+protocol TabBarRouterProtocol: BaseRouter {
+    
+}
+
+class TabBarRouter: TabBarRouterProtocol {
+    
+}
