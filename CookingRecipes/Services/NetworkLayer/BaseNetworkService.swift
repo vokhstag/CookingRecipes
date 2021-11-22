a1bb198982587da9012f7a959df6cb2b121fd0be
Yunus <vokhstag@gmail.com>
2021-11-22 13:19:21 +0300

    Выключил Host Application для тестов
    Выключил Host Application для тестов

---
+++ CookingRecipes/Services/NetworkLayer/BaseNetworkService.swift
@@ -0,0 +1,8 @@
+//
+//  BaseNetworkService.swift
+//  CookingRecipes
+//
+//  Created by Yunus Abubakarov on 22.11.2021.
+//
+
+import Foundation
---
+++ CookingRecipes/Services/NetworkLayer/NetworkServiceError.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkServiceError.swift
+//  CookingRecipes
+//
+//  Created by Yunus Abubakarov on 22.11.2021.
+//
+
+import Foundation
