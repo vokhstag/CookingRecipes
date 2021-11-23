086e8c5e6d58666c716e3f45c0110c55702d298e
Yunus <vokhstag@gmail.com>
2021-11-23 11:04:54 +0300

    Добавлен класс UserDataManager
    Добавлен класс UserDataManager для создания и получения юзера из CoreData

---
+++ CookingRecipes/Services/DataManager/UserDataManager.swift
@@ -0,0 +1,39 @@
+//
+//  UserDataManager.swift
+//  CookingRecipes
+//
+//  Created by Yunus Abubakarov on 22.11.2021.
+//
+
+import Foundation
+import CoreData
+
+protocol UserDataManagerProtocol {
+    func getActiveUser() -> User?
+    func createUser(name: String, login: String, password: String) -> Bool
+}
+
+class UserDataManager {
+    // MARK: - Private properties
+    private let context = AppDelegate.viewContext
+    private let fetchResult: NSFetchRequest<User> = User.fetchRequest()
+    func getActiveUser() -> User? {
+        let predicate = NSPredicate(format: "isActive = %@", true)
+        fetchResult.predicate = predicate
+        let users = try? context.fetch(fetchResult)
+        return users?.first
+    }
+    func createUser(name: String, login: String, password: String) -> Bool {
+        let user = User(context: context)
+        user.name = name
+        user.login = login
+        user.password = password
+        user.isActive = true
+        do {
+            try context.save()
+            return true
+        } catch {
+            return false
+        }
+    }
+}
---
+++ CookingRecipes/📱 Screens/AuthorizationModule/Presenter/AuthorizationPresentor.swift
@@ -0,0 +1,8 @@
+//
+//  AuthorizationPresentor.swift
+//  CookingRecipes
+//
+//  Created by Yunus Abubakarov on 23.11.2021.
+//
+
+import Foundation
