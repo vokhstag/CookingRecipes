d2a2bc5c6af423fb4b34fcbb9807a3005a35af34
Yunus <vokhstag@gmail.com>
2021-11-23 11:05:35 +0300

    Добавлен AuthorizationViewController
    Добавлен AuthorizationViewController и презентер к нему

--- CookingRecipes/📱 Screens/AuthorizationModule/Presenter/AuthorizationPresentor.swift
+++ CookingRecipes/📱 Screens/AuthorizationModule/Presenter/AuthorizationPresentor.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+protocol AuthorizationViewProtocol: class {
+    func succes()
+    func failure(errorDescription: String)
+}
+
+protocol AuthorizationPresenterProtocol {
+    func createNewUser(name: String, login: String, password: String)
+}
+
+class AuthorizationPresenter {
+    private weak var view: AuthorizationViewProtocol?
+    private let dataManager: UserDataManagerProtocol
+    // MARK: - Constructor
+    init(view: AuthorizationViewProtocol, dataManager: UserDataManagerProtocol) {
+        self.view = view
+        self.dataManager = dataManager
+    }
+}
+//
+extension AuthorizationPresenter: AuthorizationPresenterProtocol {
+    func createNewUser(name: String, login: String, password: String) {
+        let result = dataManager.createUser(name: name, login: login, password: password)
+        if result {
+            view?.succes()
+        } else {
+            view?.failure(errorDescription: "Не удалось зарегистрироваться")
+        }
+    }
+}
